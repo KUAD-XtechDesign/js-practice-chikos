07611c59cc57c99bddf44ab6c6f4bea41656572a
chikos <68629026+chikos@users.noreply.github.com>
2020-10-16 14:11:14 +0900

    pp

---
+++ sample/js/script01.js
@@ -0,0 +1,15 @@
+$(function(){
+   
+    $("#button01").on("click",function(){
+        $("#box01").hide();
+    })
+    
+    $("#button01").on("click",function(){
+        $("#box04").hide();
+    })
+    
+    $("#button01").on("click",function(){ 
+        $("#box07").hide();   
+    })
+
+})
